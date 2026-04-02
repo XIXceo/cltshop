4c2d783af44ffbcecc993e90d9a819cbc69a3933
Your Name <youremail@example.com>
2026-04-02 14:18:01 +0200

    Fix NextRequest type in variant API

--- app/api/products/variant/[id]/route.ts
+++ app/api/products/variant/[id]/route.ts
@@ -1,8 +1,9 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
 export async function GET(
-  req: Request,
+  req: NextRequest,
   { params }: { params: { id: string } }
 ) {
   try {
